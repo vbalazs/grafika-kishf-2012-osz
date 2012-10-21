096c294abf3b41363f11c2524761b4b1735d6f42
Balazs Varga <balo.software@gmail.com>
2012-10-21 13:02:27 +0200

    Második kisházi kezdeti verzió: néhány alap konstans, viewport beállítás, fibonacci sor generálás a paraméterekhez

---
+++ hf2/khf2.cpp
@@ -0,0 +1,267 @@
+//=============================================================================================
+// Szamitogepes grafika hazi feladat keret. Ervenyes 2012-tol.
+// A //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// sorokon beluli reszben celszeru garazdalkodni, mert a tobbit ugyis toroljuk.
+// A beadott program csak ebben a fajlban lehet, a fajl 1 byte-os ASCII karaktereket tartalmazhat.
+// Tilos:
+// - mast "beincludolni", illetve mas konyvtarat hasznalni
+// - faljmuveleteket vegezni (printf is fajlmuvelet!)
+// - new operatort hivni az onInitialization f�ggv�nyt kiv�ve, a lefoglalt adat korrekt felszabad�t�sa n�lk�l
+// - felesleges programsorokat a beadott programban hagyni
+// - tovabbi kommenteket a beadott programba irni a forrasmegjelolest kommentjeit kiveve
+// ---------------------------------------------------------------------------------------------
+// A feladatot ANSI C++ nyelvu forditoprogrammal ellenorizzuk, a Visual Studio-hoz kepesti elteresekrol
+// es a leggyakoribb hibakrol (pl. ideiglenes objektumot nem lehet referencia tipusnak ertekul adni)
+// a hazibeado portal ad egy osszefoglalot.
+// ---------------------------------------------------------------------------------------------
+// A feladatmegoldasokban csak olyan gl/glu/glut fuggvenyek hasznalhatok, amelyek
+// 1. Az oran a feladatkiadasig elhangzottak ES (logikai AND muvelet)
+// 2. Az alabbi listaban szerepelnek:
+// Rendering pass: glBegin, glVertex[2|3]f, glColor3f, glNormal3f, glTexCoord2f, glEnd, glDrawPixels
+// Transzformaciok: glViewport, glMatrixMode, glLoadIdentity, glMultMatrixf, gluOrtho2D,
+// glTranslatef, glRotatef, glScalef, gluLookAt, gluPerspective, glPushMatrix, glPopMatrix,
+// Illuminacio: glMaterialfv, glMaterialfv, glMaterialf, glLightfv
+// Texturazas: glGenTextures, glBindTexture, glTexParameteri, glTexImage2D, glTexEnvi,
+// Pipeline vezerles: glShadeModel, glEnable/Disable a kovetkezokre:
+// GL_LIGHTING, GL_NORMALIZE, GL_DEPTH_TEST, GL_CULL_FACE, GL_TEXTURE_2D, GL_BLEND, GL_LIGHT[0..7]
+//
+// NYILATKOZAT
+// ---------------------------------------------------------------------------------------------
+// Nev    : Varga Balazs
+// Neptun : IFAW8V
+// ---------------------------------------------------------------------------------------------
+// ezennel kijelentem, hogy a feladatot magam keszitettem, es ha barmilyen segitseget igenybe vettem vagy
+// mas szellemi termeket felhasznaltam, akkor a forrast es az atvett reszt kommentekben egyertelmuen jeloltem.
+// A forrasmegjeloles kotelme vonatkozik az eloadas foliakat es a targy oktatoi, illetve a
+// grafhazi doktor tanacsait kiveve barmilyen csatornan (szoban, irasban, Interneten, stb.) erkezo minden egyeb
+// informaciora (keplet, program, algoritmus, stb.). Kijelentem, hogy a forrasmegjelolessel atvett reszeket is ertem,
+// azok helyessegere matematikai bizonyitast tudok adni. Tisztaban vagyok azzal, hogy az atvett reszek nem szamitanak
+// a sajat kontribucioba, igy a feladat elfogadasarol a tobbi resz mennyisege es minosege alapjan szuletik dontes.
+// Tudomasul veszem, hogy a forrasmegjeloles kotelmenek megsertese eseten a hazifeladatra adhato pontokat
+// negativ elojellel szamoljak el es ezzel parhuzamosan eljaras is indul velem szemben.
+//=============================================================================================
+
+#include <math.h>
+#include <stdlib.h>
+
+#if defined(WIN32) || defined(_WIN32) || defined(__WIN32__)
+// MsWindows-on ez is kell
+#include <windows.h>
+#endif // Win32 platform
+
+#include <GL/gl.h>
+#include <GL/glu.h>
+// A GLUT-ot le kell tolteni: http://www.opengl.org/resources/libraries/glut/
+#include <GL/glut.h>
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// Innentol modosithatod...
+
+//--------------------------------------------------------
+// 3D Vektor
+//--------------------------------------------------------
+
+struct Vector {
+    float x, y, z;
+
+    Vector() {
+        x = y = z = 0;
+    }
+
+    Vector(float x0, float y0, float z0 = 0) {
+        x = x0;
+        y = y0;
+        z = z0;
+    }
+
+    Vector operator*(float a) {
+        return Vector(x * a, y * a, z * a);
+    }
+
+    Vector operator+(const Vector & v) {
+        return Vector(x + v.x, y + v.y, z + v.z);
+    }
+
+    Vector operator-(const Vector & v) {
+        return Vector(x - v.x, y - v.y, z - v.z);
+    }
+
+    float operator*(const Vector & v) { // dot product
+        return (x * v.x + y * v.y + z * v.z);
+    }
+
+    Vector operator%(const Vector & v) { // cross product
+        return Vector(y * v.z - z * v.y, z * v.x - x * v.z, x * v.y - y * v.x);
+    }
+
+    float Length() {
+        return sqrt(x * x + y * y + z * z);
+    }
+};
+
+//--------------------------------------------------------
+// Spektrum illetve szin
+//--------------------------------------------------------
+
+struct Color {
+    float r, g, b;
+
+    Color() {
+        r = g = b = 0;
+    }
+
+    Color(float r0, float g0, float b0) {
+        r = r0;
+        g = g0;
+        b = b0;
+    }
+
+    Color operator*(float a) {
+        return Color(r * a, g * a, b * a);
+    }
+
+    Color operator*(const Color & c) {
+        return Color(r * c.r, g * c.g, b * c.b);
+    }
+
+    Color operator+(const Color & c) {
+        return Color(r + c.r, g + c.g, b + c.b);
+    }
+};
+
+const int screenWidth = 600;
+const int screenHeight = 600;
+
+const int VIRT_WIDTH = 1000; //1000mm
+const Vector VIRTCAM_BOTTOM_LEFT = Vector(100, 100);
+const Vector VIRTCAM_TOP_RIGHT = Vector(500, 500);
+
+const int NR_OF_CTRPs = 100; //number of control points
+
+//fibonacci
+double fibonacci[NR_OF_CTRPs];
+
+Color image[screenWidth*screenHeight];
+
+long time = 0;
+
+/*
+ * Binet form
+ * Forras: https://github.com/vbalazs/grafika-kishf-2012-tavasz/
+ * (tavalyi hazimbol)
+ */
+const double getFibonacciNr(int n) {
+    const double sqrt5 = sqrt(5);
+    return (pow(1 + sqrt5, n) - pow(1 - sqrt5, n)) / (sqrt5 * pow(2, n));
+}
+
+const bool fequals(float f1, float f2) {
+    if (fabs(f1 - f2) < 0.001) return true;
+    return false;
+}
+
+void ns_glVertex2Vectors(const Vector vectors[], const int size) {
+    for (int i = 0; i < size; i++) {
+        glVertex2f(vectors[i].x, vectors[i].y);
+    }
+}
+
+void onInitialization() {
+    glViewport(0, 0, screenWidth, screenHeight);
+
+    glLoadIdentity();
+
+    gluOrtho2D(VIRTCAM_BOTTOM_LEFT.x, //left
+            VIRTCAM_TOP_RIGHT.x, //right
+            VIRTCAM_BOTTOM_LEFT.y, //bottom
+            VIRTCAM_TOP_RIGHT.y); //top
+
+    //fill up array with fibonacci numbers - Binet form
+    for (int i = 2; i <= NR_OF_CTRPs; i++) {
+        fibonacci[i - 1] = getFibonacciNr(i);
+    }
+}
+
+void onDisplay() {
+    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glDrawPixels(screenWidth, screenHeight, GL_RGB, GL_FLOAT, image);
+
+    //draw
+
+    glutSwapBuffers();
+
+}
+
+void onKeyboard(unsigned char key, int x, int y) {
+    if (key == 'd') glutPostRedisplay();
+    if (key == 'q') exit(0);
+}
+
+void onMouse(int button, int state, int x, int y) {
+    //    if (button == GLUT_LEFT && state == GLUT_DOWN) {
+    //    } else if (button == GLUT_RIGHT_BUTTON && state == GLUT_DOWN) {
+    //    }
+
+    glutPostRedisplay();
+}
+
+void simulateWorld(long tstart, long tend) {
+    const static double DT_100MS = 100;
+    //    timeTmp += tend - tstart;
+    //
+    //    if (timeTmp >= DT_100MS) {
+    //        for (long ts = timeTmp; timeTmp > 0; timeTmp -= DT_100MS) {
+    //
+    //            stepSign(centerHansel, angleHansel, DT_100MS / 1000);
+    //            stepSign(centerGreta, angleGreta, DT_100MS / 1000);
+    //
+    //            calcCoverage();
+    //
+    //            if (isCovered) {
+    //                timeCovered += DT_100MS;
+    //            }
+    //        }
+    //    }
+}
+
+void onIdle() {
+    long old_time = time;
+    time = glutGet(GLUT_ELAPSED_TIME);
+
+    simulateWorld(old_time, time);
+
+    glutPostRedisplay();
+}
+
+// ...Idaig modosithatod
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+// A C++ program belepesi pontja, a main fuggvenyt mar nem szabad bantani
+
+int main(int argc, char **argv) {
+    glutInit(&argc, argv); // GLUT inicializalasa
+    glutInitWindowSize(600, 600); // Alkalmazas ablak kezdeti merete 600x600 pixel
+    glutInitWindowPosition(100, 100); // Az elozo alkalmazas ablakhoz kepest hol tunik fel
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_DEPTH); // 8 bites R,G,B,A + dupla buffer + melyseg buffer
+
+    glutCreateWindow("Grafika hazi feladat"); // Alkalmazas ablak megszuletik es megjelenik a kepernyon
+
+    glMatrixMode(GL_MODELVIEW); // A MODELVIEW transzformaciot egysegmatrixra inicializaljuk
+    glLoadIdentity();
+    glMatrixMode(GL_PROJECTION); // A PROJECTION transzformaciot egysegmatrixra inicializaljuk
+    glLoadIdentity();
+
+    onInitialization(); // Az altalad irt inicializalast lefuttatjuk
+
+    glutDisplayFunc(onDisplay); // Esemenykezelok regisztralasa
+    glutMouseFunc(onMouse);
+    glutIdleFunc(onIdle);
+    glutKeyboardFunc(onKeyboard);
+
+    glutMainLoop(); // Esemenykezelo hurok
+
+    return 0;
+}
+
