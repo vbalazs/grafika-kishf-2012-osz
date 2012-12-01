8d86f6513a21be37d1e9cca6406fb02df6397e41
Balazs Varga <balo.software@gmail.com>
2012-12-01 10:55:57 +0100

    valami már van.... helikopter törzs ellipszisből, terep

--- hf4/khf4.cpp
+++ hf4/khf4.cpp
@@ -48,10 +48,11 @@
 #include <GL/glu.h>
 #include <GL/glut.h>
 
-//gyilok!
+//torlendo
 #include <iostream>
 using namespace std;
 //
+// <editor-fold defaultstate="collapsed" desc="osztalyok, strukturak">
 
 struct Vector {
     float x, y, z;
@@ -64,6 +65,18 @@ struct Vector {
         x = x0;
         y = y0;
         z = z0;
+    }
+
+    static Vector X() {
+        return Vector(1.0f, 0.0f, 0.0f);
+    }
+
+    static Vector Y() {
+        return Vector(0.0f, 1.0f, 0.0f);
+    }
+
+    static Vector Z() {
+        return Vector(0.0f, 0.0f, 1.0f);
     }
 
     Vector operator*(float a) {
@@ -88,6 +101,10 @@ struct Vector {
 
     float Length() {
         return sqrt(x * x + y * y + z * z);
+    }
+
+    void dump() {
+        cout << "x,y,z=" << x << "; " << y << "; " << z << endl;
     }
 };
 
@@ -117,43 +134,347 @@ struct Color {
     }
 };
 
+/*
+ * forras: https://github.com/tmichel/Grafika-hf-2011-osz/blob/master/khf4.cpp#L889
+ */
+class Camera {
+public:
+    Vector pos, dir, up;
+
+    Camera() {
+        pos = dir = Vector(0.0, 0.0, 0.0);
+        up = Vector::Y();
+    }
+
+    void lookAt() {
+        gluLookAt(pos.x, pos.y, pos.z, dir.x, dir.y, dir.z, up.x, up.y, up.z);
+    }
+
+    void dump() {
+        cout << "pos=" << pos.x << "; " << pos.y << "; " << pos.z << " || ";
+        cout << "dir=" << dir.x << "; " << dir.y << "; " << dir.z << " || ";
+        cout << "up=" << up.x << "; " << up.y << "; " << up.z << endl;
+    }
+};
+// </editor-fold>
+
 const int screenWidth = 600;
 const int screenHeight = 600;
 
 Color image[screenWidth*screenHeight];
 
+Camera cam;
+Vector sunPos;
+unsigned int fieldTexture;
+
+const int FIELD_WIDTH = 30;
+const int FIELD_LONG = 50;
+
+void drawChopper() {
+    const GLfloat diff[] = {0.623529, 0.623529, 0.372549, 1.0};
+    const GLfloat spec[] = {0.623529, 0.623529, 0.372549, 1.0};
+    const GLfloat amb[] = {0.4, 0.4, 0.4, 1.0};
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diff);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
+    glMaterialf(GL_FRONT, GL_SHININESS, 30);
+
+    const float fA = 0.4;
+    const float fB = 0.6;
+    const float fC = 0.3;
+
+    const float tStep = (M_PI) / 60.0;
+    const float sStep = (M_PI) / 50.0;
+
+    glPushMatrix();
+    glRotatef(90, 1, 0, 0);
+    glRotatef(180, 0, 0, 1);
+    glBegin(GL_TRIANGLE_STRIP);
+    for (float t = -M_PI / 2; t <= (M_PI / 2) + 0.0001; t += tStep) {
+        if (t <= M_PI && t >= -M_PI * 0.5) {
+            for (float s = -M_PI; s <= M_PI + 0.0001; s += sStep) {
+                if (s <= 0) {
+                    glNormal3f(fA * cos(t) * cos(s), fB * cos(t) * sin(s), fC * sin(t));
+                    glVertex3f(fA * cos(t) * cos(s), fB * cos(t) * sin(s), fC * sin(t));
+
+                    glNormal3f(fA * cos(t + tStep) * cos(s), fB * cos(t + tStep) * sin(s), fC * sin(t + tStep));
+                    glVertex3f(fA * cos(t + tStep) * cos(s), fB * cos(t + tStep) * sin(s), fC * sin(t + tStep));
+                }
+            }
+        }
+
+        if (t >= 0) {
+            for (float s = -M_PI; s <= M_PI + 0.0001; s += sStep) {
+                if (s >= -0.1) {
+                    glNormal3f(fA * cos(t) * cos(s), fB * cos(t) * sin(s), fC * sin(t));
+                    glVertex3f(fA * cos(t) * cos(s), fB * cos(t) * sin(s), fC * sin(t));
+
+                    glNormal3f(fA * cos(t + tStep) * cos(s), fB * cos(t + tStep) * sin(s), fC * sin(t + tStep));
+                    glVertex3f(fA * cos(t + tStep) * cos(s), fB * cos(t + tStep) * sin(s), fC * sin(t + tStep));
+                }
+            }
+        }
+    }
+    glEnd();
+
+    //üveg
+    const GLfloat glass_diff[] = {1.0, 1.0, 1.0, 0.3};
+    const GLfloat glass_spec[] = {0.0, 0.0, 1.0, 0.3};
+    const GLfloat glass_amb[] = {0.0, 0.0, 1.0, 0.3};
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT, glass_amb);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, glass_diff);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, glass_spec);
+    glMaterialf(GL_FRONT, GL_SHININESS, 40);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glBegin(GL_TRIANGLE_STRIP);
+    for (float t = -M_PI / 2; t <= (M_PI / 2) + 0.0001; t += tStep) {
+        if (t <= 0) {
+            for (float s = -M_PI; s <= M_PI + 0.0001; s += sStep) {
+                if (s >= -0.1) {
+                    glNormal3f(fA * cos(t) * cos(s), fB * cos(t) * sin(s), fC * sin(t));
+                    glVertex3f(fA * cos(t) * cos(s), fB * cos(t) * sin(s), fC * sin(t));
+
+                    glNormal3f(fA * cos(t + tStep) * cos(s), fB * cos(t + tStep) * sin(s), fC * sin(t + tStep));
+                    glVertex3f(fA * cos(t + tStep) * cos(s), fB * cos(t + tStep) * sin(s), fC * sin(t + tStep));
+                }
+            }
+        }
+    }
+    glEnd();
+    glDisable(GL_BLEND);
+    glPopMatrix();
+}
+
+void drawSphere() {
+    glPushMatrix();
+    glTranslatef(sunPos.x, sunPos.y, sunPos.z);
+    glScalef(0.1, 0.1, 0.1);
+    static const float d = 0.02f;
+
+    const GLfloat diff[] = {1.0, 0.0, 0.6, 1.0};
+    const GLfloat spec[] = {1.0, 0.85, 0.6, 1.0};
+    const GLfloat amb[] = {0.4, 0.4, 0.4, 1.0};
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diff);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
+    glMaterialf(GL_FRONT, GL_SHININESS, 100);
+
+    glBegin(GL_QUADS);
+    for (float u = 0.0f; u < 1.0f; u += d) {
+        for (float v = 0.0f; v < 1.0f; v += d) {
+            float x = cos(2 * M_PI * u) * sin(M_PI * v);
+            float y = sin(2 * M_PI * u) * sin(M_PI * v);
+            float z = cos(M_PI * v);
+            glNormal3f(x, y, z);
+            glVertex3f(x, y, z);
+
+            x = cos(2 * M_PI * (u)) * sin(M_PI * (v + d));
+            y = sin(2 * M_PI * (u)) * sin(M_PI * (v + d));
+            z = cos(M_PI * (v + d));
+            glNormal3f(x, y, z);
+            glVertex3f(x, y, z);
+
+            x = cos(2 * M_PI * (u + d)) * sin(M_PI * (v + d));
+            y = sin(2 * M_PI * (u + d)) * sin(M_PI * (v + d));
+            z = cos(M_PI * (v + d));
+            glNormal3f(x, y, z);
+            glVertex3f(x, y, z);
+
+            x = cos(2 * M_PI * (u + d)) * sin(M_PI * v);
+            y = sin(2 * M_PI * (u + d)) * sin(M_PI * v);
+            z = cos(M_PI * v);
+            glNormal3f(x, y, z);
+            glVertex3f(x, y, z);
+        }
+    }
+    glEnd();
+    glPopMatrix();
+}
+
+void drawField() {
+    static const float d = 0.02f;
+
+    //    glEnable(GL_TEXTURE_2D);
+    //    glBindTexture(GL_TEXTURE_2D, fieldTexture);
+
+    const GLfloat diff[] = {0.0, 0.91, 0.0, 1.0};
+    const GLfloat spec[] = {0.0, 0.01, 0.0, 1.0};
+    const GLfloat amb[] = {0.1, 0.4, 0.1, 1.0};
+
+    glMaterialfv(GL_FRONT, GL_AMBIENT, amb);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE, diff);
+    glMaterialfv(GL_FRONT, GL_SPECULAR, spec);
+    glMaterialf(GL_FRONT, GL_SHININESS, 10.0);
+
+    glPushMatrix();
+    glScalef(FIELD_WIDTH, 1, FIELD_LONG);
+    glTranslatef(0, -5, -1);
+    glBegin(GL_QUADS);
+    glNormal3f(0, 1, 0);
+    for (float x = -1.0f; x < 1.0f; x += d) {
+        for (float z = -1.0f; z < 1.0f; z += d) {
+            glTexCoord2f(x, z);
+            glVertex3f(x, 0, -z);
+
+            glTexCoord2f(x + d, z);
+            glVertex3f(x + d, 0, -z);
+
+            glTexCoord2f(x + d, z + d);
+            glVertex3f(x + d, 0, -(z + d));
+
+            glTexCoord2f(x, z + d);
+            glVertex3f(x, 0, -(z + d));
+        }
+    }
+
+    glEnd();
+    glPopMatrix();
+    //    glDisable(GL_TEXTURE_2D);
+}
+
+void setLight(Vector sunPosv) {
+    float ambient[] = {0.5, 0.5, 0.5, 1.0};
+    float sunColor[] = {0.75, 0.75, 0.67, 1.0};
+    float glSunPos[] = {sunPosv.x, sunPosv.y, sunPosv.z, 0};
+
+    cout << "sunPos: ";
+    sunPosv.dump();
+
+    glDisable(GL_COLOR_MATERIAL);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, sunColor);
+    glLightfv(GL_LIGHT0, GL_POSITION, glSunPos);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, sunColor);
+
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+}
+
+void initFieldTexture() {
+    /*
+     * Forras: https://github.com/tmichel/Grafika-hf-2011-osz/blob/master/khf4.cpp#L919
+     */
+    unsigned char image[FIELD_WIDTH][FIELD_LONG][3];
+    unsigned int tex[1];
+    glGenTextures(1, tex);
+
+    glBindTexture(GL_TEXTURE_2D, tex[0]);
+    glTexEnvi(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_MODULATE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    for (int x = 0; x < FIELD_WIDTH; ++x) {
+        for (int y = 0; y < FIELD_LONG; ++y) {
+            image[x][y][0] = 51 - (x + y) * 10;
+            image[x][y][1] = 58 - (x - y) * 59;
+            image[x][y][2] = 16 - (x * y) * 15;
+        }
+    }
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, FIELD_WIDTH, FIELD_LONG, 0,
+            GL_RGB, GL_UNSIGNED_BYTE, image);
+
+    fieldTexture = tex[0];
+}
+
 void onInitialization() {
     glViewport(0, 0, screenWidth, screenHeight);
 
+    initFieldTexture();
+
     glMatrixMode(GL_PROJECTION);
+    gluPerspective(60.0, 1.0, 0.1, FIELD_LONG);
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
+
+    glEnable(GL_DEPTH_TEST);
+    glDisable(GL_CULL_FACE);
+    glEnable(GL_NORMALIZE);
+    glShadeModel(GL_SMOOTH);
+    glDisable(GL_TEXTURE_2D);
+
+    cam.pos = Vector(0.0, 1.0, 2.0);
+    sunPos = Vector(0.0, 0.01, -5.0);
+    setLight(sunPos);
 }
 
 void onDisplay() {
-    glClearColor(0.1f, 0.2f, 0.3f, 1.0f);
+    glClearColor(0.6f, 0.77f, 0.75f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glutSwapBuffers();
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+
+    cam.lookAt();
+
+    drawSphere();
+
+    drawField();
+
+    drawChopper();
+
+    glutSwapBuffers(); // Buffercsere: rajzolas vege
+
 }
 
 void onKeyboard(unsigned char key, int x, int y) {
-    if (key == 'd') glutPostRedisplay();
-    if (key == 'q') exit(0);
+    if (key == 'q') {
+        exit(0);
+    }
+
+    //Roll
+    if (key == 'R') {
+        //növel 20 fokkal
+    }
+    if (key == 'E') {
+        //csökkent 20 fokkal
+    }
+
+    //Pitch
+    if (key == 'P') {
+        //növel 20 fokkal
+    }
+    if (key == 'O') {
+        //csökkent 20 fokkal
+    }
+
+    //Yaw
+    if (key == 'Y') {
+        //növel 20 fokkal
+    }
+    if (key == 'X') {
+        //csökkent 20 fokkal
+    }
+
+    //--- törlendõ, csak debug
+    if (key == 't') {
+        cam.pos = Vector(0.0, 1.0, 2.0);
+        cam.dump();
+    }
+
+    if (key == 'c') {
+        cam.pos.x -= 1;
+        cam.dump();
+    }
+
+    if (key == 'v') {
+        cam.pos.x += 1;
+        cam.dump();
+    }
+
+    glutPostRedisplay();
 }
 
 void onMouse(int button, int state, int x, int y) {
-    if (button == GLUT_LEFT && state == GLUT_DOWN)
-        glutPostRedisplay();
 }
 
 void onIdle() {
-    long time = glutGet(GLUT_ELAPSED_TIME);
 }
-
-// ...Idaig modosithatod
-//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// A C++ program belepesi pontja, a main fuggvenyt mar nem szabad bantani
 
 int main(int argc, char **argv) {
     glutInit(&argc, argv); // GLUT inicializalasa
